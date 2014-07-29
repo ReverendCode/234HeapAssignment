890d2cebf9e4a6ce58b9f4333bcd9e1d5856053d
ReverendCode <codenull@gmail.com>
2014-07-29 13:55:09 -0700

    still building.

--- src/com/vaporware/heaps/Heap.java
+++ src/com/vaporware/heaps/Heap.java
@@ -27,16 +27,58 @@ public class Heap {
         int parentLoc;
         //start at the last item added, go up the tree to it's proper spot
         //index/2-1 to get parent
-        if ((lastItem & 1) == 0) {
-            parentLoc = lastItem/2;
+        for (int index=lastItem;index>0;) {
+            if ((index & 1) == 0) parentLoc = index/2;//even case
+        else parentLoc = index/2 -1;//odd case
+            
+            if (theHeap[parentLoc] > theHeap[index]) {
+                swap(index, parentLoc); 
+                index=parentLoc;
+            }
+            else return;
         }
-        else parentLoc = lastItem/2 -1;//odd case
 
     }
     private void filterDown() {//this will overwrite the 'root'
         //move last item up, then filter down
+        boolean smallerLeft;
+        theHeap[0] = theHeap[lastItem--];
+        for (int index = 0;index <= lastItem;) {
+            int leftChild = index*2+1;
+            int rightChild = index*2+2;
+            if (leftChild >lastItem) return; //if left is out of range, right is Right out.
+            if (rightChild > lastItem) {
+                if (theHeap[leftChild] < theHeap[index]) {
+                    swap(leftChild, index);
+                    index = leftChild;
+                }
+                else return;
+            }//end leftChild only case
+            else {
+                if (theHeap[leftChild] < theHeap[rightChild]) smallerLeft = true;
+                else smallerLeft = false;
+
+                if (smallerLeft) {
+                    if (theHeap[leftChild] < theHeap[index]) {
+                        swap(leftChild,index);
+                        index = leftChild;
+                    }
+                }
+                else {
+                    if (theHeap[rightChild] <= theHeap[index]) {
+                        swap(rightChild,index);
+                        index=rightChild;
+                    }
+
+                }
+            }
+            
+        }
     }
 
+    
+    
+    
     private void swap(int firstLoc, int secondLoc) {
         int temp = theHeap[firstLoc];
         theHeap[firstLoc]=theHeap[secondLoc];
